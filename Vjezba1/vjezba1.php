78bc423d6de2f11f8d498beb12fe648b754c6caa
apinjusic <91885729+apinjusic@users.noreply.github.com>
2023-12-08 13:53:47 +0100

    Create vjezba1.php

---
+++ Vjezba1/vjezba1.php
@@ -0,0 +1,24 @@
+<?php
+print '
+<!DOCTYPE HTML>
+<html lang="hr">
+	<head>
+		<title>Da Vincijev kod</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Andrej Pinjušić">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	</head>
+<body>
+	<h1>Da Vincijev kod</h1>
+	<p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.<br>
+	<a href="https://hr.wikipedia.org/Da_Vincijev_kod" target="_blank">Wikipedia</a></p>
+</body>
+</html>
+<!-- VJEZBA1 -->';
+
+
+
+?>
