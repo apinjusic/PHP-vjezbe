aca1d71551275dc79178440c17f0f82c2cf3c8b6
apinjusic <91885729+apinjusic@users.noreply.github.com>
2023-12-08 13:55:16 +0100

    Create vjezba3.php

---
+++ Vjezba3/vjezba3.php
@@ -0,0 +1,27 @@
+<?php
+$title = "Da Vincijev kod";
+$link = "https://hr.wikipedia.org/Da_Vincijev_kod" ;
+
+echo '
+<!DOCTYPE HTML>
+<html lang="hr">
+	<head>
+		<title>Da Vincijev kod</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Andrej Pinjušić">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	</head>
+<body>
+	<h1>' . $title . '</h1>
+	<p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.<br>
+	<a href=\"" .$link. "\" target=\"_blank\">' . $link . "</a></p>
+</body>
+</html>
+<!-- VJEZBA3 -->"
+
+
+
+?>
